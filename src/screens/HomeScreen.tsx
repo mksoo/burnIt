ab8df14f5b15ef83cdc4ff2ba8fa39506768c4da
mksoo <mgs3067@naver.com>
2025-07-09 01:17:22 +0900

    HomeScreen 스타일 수정

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,23 @@
-import { FC } from "react"
-import { Text, View } from "react-native"
-import { MainTabScreenProps, RootStackScreenProps } from "./navigation";
+import { FC } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { MainTabScreenProps, RootStackScreenProps } from './navigation';
 
-const HomeScreen: FC<MainTabScreenProps<'Home'>>= () => {
-    return (
-        <View style={{
-            backgroundColor: 'red',
-        }}>
-            <Text>
-                text
-            </Text>
-        </View>
-    )
-}
+const HomeScreen: FC<MainTabScreenProps<'Home'>> = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Home text</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default HomeScreen;
