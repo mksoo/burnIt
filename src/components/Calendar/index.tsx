2f3d907da3ba1b0f45aeb4ee74f38c976356f2bf
mksoo <mgs3067@naver.com>
2025-07-10 19:58:12 +0900

    제스처 테스트

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -5,8 +5,9 @@ import { CalendarHeader } from './CalendarHeader';
 import { WeekDayHeader } from './WeekDayHeader';
 import CalendarMonthGridView from './CalendarMonthGridView';
 import { styles } from './styles';
-import CalendarWeekView from './CalendarWeekView';
 import Divider from '../common/Divider';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 const Calendar: FC = () => {
   const {
@@ -22,6 +23,20 @@ const Calendar: FC = () => {
     viewType,
   } = useCalendar();
 
+  const position = useSharedValue(0);
+
+  const gesture = Gesture.Pan()
+    .onUpdate((e) => {
+      position.value = e.translationY;
+      console.log(position.value, e.translationY);
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    height: position.value,
+  }));
+
+
+
   return (
     <View style={styles.container}>
       <CalendarHeader
@@ -31,7 +46,15 @@ const Calendar: FC = () => {
       />
       <View style={styles.calendarContainer}>
         <WeekDayHeader />
-        {viewType==="MONTH" ? 
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[animatedStyle]}>
+          <CalendarMonthGridView 
+            selectedDay={selectedDay}
+            monthDays={monthDays} 
+            handleDayPress={handleDayPress}/>
+          </Animated.View>
+        </GestureDetector>
+        {/* {viewType==="MONTH" ? 
           <CalendarMonthGridView 
             selectedDay={selectedDay}
             monthDays={monthDays} 
@@ -41,7 +64,7 @@ const Calendar: FC = () => {
             weekDays={weekDays}
             selectedDay={selectedDay}
             setSelectedDay={setSelectedDay}/>
-        }
+        } */}
       </View>
       <Divider style={{marginTop: 10}}/>
     </View>
